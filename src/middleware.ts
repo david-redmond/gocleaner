c5d3f4a9e1eaeb47f3e34abc225992505c8d2986
david-redmond <dredmond@wynshop.com>
2024-11-28 02:29:34 +0000

    fixed lint

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
-export function middleware(req) {
+export function middleware(req: any) {
     if (process.env.NODE_ENV === 'development') {
         return NextResponse.next();
     }
